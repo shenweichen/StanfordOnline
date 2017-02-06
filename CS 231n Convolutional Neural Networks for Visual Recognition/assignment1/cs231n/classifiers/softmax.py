1b4c38e5fc87ba9428da103d32234606b104a80a
shenweichen <last.fantasy@qq.com>
2017-02-06 22:23:00 +0800

    finish A1_Q3softmax
    assignment1:
    Q3: Implement a Softmax classifier

--- CS 231n Convolutional Neural Networks for Visual Recognition/assignment1/cs231n/classifiers/softmax.py
+++ CS 231n Convolutional Neural Networks for Visual Recognition/assignment1/cs231n/classifiers/softmax.py
@@ -22,7 +22,7 @@ def softmax_loss_naive(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
-
+  dW_each = np.zeros_like(W)
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
@@ -30,6 +30,24 @@ def softmax_loss_naive(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   pass
+  n_train,n_class = X.shape[0],W.shape[1]
+  f = np.dot(X,W) 
+  f_max = np.max(f,axis = 1).reshape(n_train,1)
+  f = f - f_max
+  for i in range(n_train):
+    score = np.exp(f[i,y[i]])/np.sum(np.exp(f[i]))
+    loss += -np.log(score)
+    for k in range(n_class):
+    	if k == y[i]:
+    		dW_each[:,k] = X[i].T * (score - 1)
+    	else:
+    		score_k = np.exp(f[i,k])/np.sum(np.exp(f[i]))
+    		dW_each[:,k] = X[i].T * score_k
+    dW += dW_each
+  loss /= n_train
+  loss += 0.5 * reg * np.sum(np.square(W))
+  dW /= n_train
+  dW += reg * W 
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -54,6 +72,16 @@ def softmax_loss_vectorized(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   pass
+  n_train,n_feature = X.shape
+  f = np.dot(X,W)
+  f_max = np.max(f,axis=1).reshape(n_train,1)
+  f = f - f_max
+  prob = np.exp(f)/np.sum(np.exp(f),axis=1,keepdims = True)
+  y_true = np.zeros_like(prob)
+  y_true[range(n_train),y] = 1.0
+  loss += -np.sum(y_true * np.log(prob))/n_train + 0.5 * reg * np.sum(W*W)
+  dW += -np.dot(X.T, y_true - prob) / n_train + reg * W
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
